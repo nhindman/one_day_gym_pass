9223c7faedf988d29a2c9037139674a9eacdaace
nhindman <nathaniel.hindman@gmail.com>
2014-01-16 16:29:03 -0500

    gym results table header is separate from table with rounded corners

--- app/controllers/passes_controller.rb
+++ app/controllers/passes_controller.rb
@@ -25,7 +25,10 @@ end
 def index
   user_input_address = params[:user_input_address]
   @gyms_near_address = user_input_address.split.map(&:capitalize).join(' ')
-  user_address = user_input_address.gsub(" ", '%20')
+  puts "LOOKING FOR ADDRESS"
+  puts user_input_address
+  user_address = user_input_address.gsub(' ','%20')
+
   # binding.pry
   thing = "http://maps.googleapis.com/maps/api/geocode/json?address=#{user_address}&sensor=true"
   address = HTTParty.get(thing)
@@ -38,19 +41,13 @@ def index
   consumer_secret = 'sBQoF_oIEJ4g3_tZyTXPFJdA-6Y'
   token = 'Ba221Wynsy_cKXZVFUCMzH8qXYbsYJq8'
   token_secret = 'GXdHVOG8J9lgiTXq34BLoBEmrDg'
-
+  
   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
   access_token = OAuth::AccessToken.new(consumer, token, token_secret)
   path = "/v2/search?term=gym&ll=#{lat},#{lng}"
   result = access_token.get(path).body
   results = JSON(result)
   @businesses = results["businesses"]
-  # foursquare venue search:
-  # venue_search = HTTParty.get("https://api.foursquare.com/v2/venues/search?categoryId=4bf58dd8d48988d175941735&ll=#{lat},#{lng}&client_id=YRXH0LHSXPSQQPQA34I41XKQCUNAVQIF0TTNXWXQC0NUZJGD&client_secret=ENUT2HBL3TARDIIF4RMLE05WLVX0FVPN452E3OMJWJEX3D1T")
-  # @items = venue_search["response"]["groups"][0]["items"]
-  # @items = Item.all
-  # redirect_to passes_path
-  # binding.pry
   render :'passes/index'
 end
 
@@ -63,7 +60,7 @@ def show
   consumer_secret = 'sBQoF_oIEJ4g3_tZyTXPFJdA-6Y'
   token = 'Ba221Wynsy_cKXZVFUCMzH8qXYbsYJq8'
   token_secret = 'GXdHVOG8J9lgiTXq34BLoBEmrDg'
-
+  #yelp business id search  
   consumer = OAuth::Consumer.new(consumer_key, consumer_secret, {:site => "http://#{api_host}"})
   access_token = OAuth::AccessToken.new(consumer, token, token_secret)
   path = "/v2/business/#{venue_id}"
@@ -75,39 +72,13 @@ def show
   @gym_review = gym["reviews"][0]["excerpt"]
   @gym_phone = gym["phone"]
   @gym_address = gym["location"]["display_address"].join(" ").gsub("["," ")
-
+  #sending address to geocoder which is sending to google map on new_pass_path page
   @gym_map_address = @gym_address.gsub('%20'," ")
   puts "looking for address" 
   puts @gym_map_address
   results = Geocoder.search(@gym_map_address) 
   @lat = results[0].data["geometry"]["location"]["lat"]
   @lng = results[0].data["geometry"]["location"]["lng"]
-  # thing = "http://maps.googleapis.com/maps/api/geocode/json?address=#{@gym_map_address}&sensor=true"
-  # address = HTTParty.get(thing)
-  # # binding.pry
-  # puts "checking latitude"
-  # @lat = address["results"][0]["geometry"]["location"]["lat"].round(6)
-  # puts "checking latitude"
-  # puts @lat
-  # @lng = address["results"][0]["geometry"]["location"]["lng"].round(6)
-
-  #hash[0].data["geometry"]["location"]["lat"]
-  #hash[0].data["geometry"]["location"]["lng"]
-  # old foursquare api code
-  # response = HTTParty.get("https://api.foursquare.com/v2/venues/#{venue_id}?client_id=YRXH0LHSXPSQQPQA34I41XKQCUNAVQIF0TTNXWXQC0NUZJGD&client_secret=ENUT2HBL3TARDIIF4RMLE05WLVX0FVPN452E3OMJWJEX3D1T")
-  # gym = JSON.parse(response.body)
-  # @gym_name = gym["response"]["venue"]["name"]
-  # @phone_number = gym["response"]["venue"]["contact"]["formattedPhone"]
-  # @address = gym["response"]["venue"]["location"]["address"]
-  # @cross_street = gym["response"]["venue"]["location"]["crossStreet"]
-  # @city = gym["response"]["venue"]["location"]["city"]
-  # @state = gym["response"]["venue"]["location"]["state"]
-  # @postal_code = gym["response"]["venue"]["location"]["postalCode"]
-  # @website = gym["response"]["venue"]["url"]
-  # @description = gym["response"]["venue"]["description"]
-  # #example request
-  #"https://api.foursquare.com/v2/venues/4b522afaf964a5200b6d27e3?client_id=CLIENT_ID&client_secret=CLIENT_SECRET
-  #https://foursquare.com/v/equinox/4a2f3322f964a520b6981fe3
   render new_pass_path
 end
 
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+Alchemist.setup
+
 module Odgp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
